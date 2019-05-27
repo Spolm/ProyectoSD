675ba01d07702d672e617e1a18deefc5dabb2a42
Spolm <luismachadoz.12@gmail.com>
2019-05-26 20:31:03 -0400

    added attributes getters and setters to Store.java

--- src/common/Store.java
+++ src/common/Store.java
@@ -5,10 +5,45 @@
  */
 package common;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Spoon
  */
 public class Store {
+    
+    private String storeName;
+    private ArrayList<Product> prouctList;
+
+    public String getStoreName() {
+        return storeName;
+    }
+
+    public void setStoreName(String storeName) {
+        this.storeName = storeName;
+    }
+
+    public ArrayList<Product> getProuctList() {
+        return prouctList;
+    }
+
+    public void setProuctList(ArrayList<Product> prouctList) {
+        this.prouctList = prouctList;
+    }
+
+    public Store() {
+    }
+
+    public Store(String storeName) {
+        this.storeName = storeName;
+    }
+    
+    public Store(String storeName, ArrayList<Product> prouctList) {
+        this.storeName = storeName;
+        this.prouctList = prouctList;
+    }
+    
+    
     
 }
